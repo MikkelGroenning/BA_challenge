9d906bd63e014363d94ac68fd9cff72f51349606
Bogeluno <bogeluno@gmail.com>
2020-10-09 15:28:25 +0200

    Started descriptive

---
+++ src/data/Preprocces.py
@@ -0,0 +1,73 @@
+#%%
+# Package import
+import pandas as pd
+import numpy as np
+import os
+from geopy.exc import GeocoderTimedOut 
+from geopy.geocoders import Nominatim 
+
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+# Read data frame
+df = pd.read_excel(os.path.abspath('../../data/Raw/Cities.xls'), index_col=0)
+
+#%% Add longitude and latitude
+# Initialize empty lists
+longitude = [] 
+latitude = [] 
+   
+# function to find the coordinate of a given city  
+def findGeocode(city): 
+       
+    # try and catch is used to overcome 
+    # the exception thrown by geolocator 
+    # using geocodertimedout   
+    try: 
+          
+        # Specify the user_agent as your 
+        # app name it should not be none 
+        geolocator = Nominatim(user_agent="VS") 
+          
+        return geolocator.geocode(city) 
+      
+    except GeocoderTimedOut: 
+          
+        return findGeocode(city)     
+  
+# each value from city column 
+# will be fetched and sent to 
+# function find_geocode    
+for _, row in df[["City","Country"]].iterrows():
+      
+    if findGeocode(row["City"]+', '+ row["Country"]) != None: 
+           
+        loc = findGeocode(row["City"]+', '+ row["Country"]) 
+          
+        # coordinates returned from  
+        # function is stored into 
+        # two separate list 
+        latitude.append(loc.latitude) 
+        longitude.append(loc.longitude) 
+       
+    # if coordinate for a city not 
+    # found, insert "NaN" indicating  
+    # missing value  
+    else: 
+        latitude.append(np.nan) 
+        longitude.append(np.nan)
+
+# Populate the two missing manually
+latitude[102], longitude[102] = 10.156421, -67.999718
+latitude[155], longitude[155] = 27.773056, -82.639999
+
+# Add columns to data frame
+df["Latitude"] = np.array(latitude)
+df["Longitude"] = np.array(longitude)
+
+
+df.to_csv(os.path.abspath('../../data/Processed/Cities.csv'))
+
+print("Done")
+# %%
